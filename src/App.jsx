7b3eec335b7f728bee7678cb4f2850f237ae0464
João Amaral <joaomramaral42@gmail.com>
2025-02-11 16:59:47 +0000

    sera ?

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Homepage from './pages/Homepage';
+import Catering from './pages/Catering';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
 
 function App() {
   return (
@@ -9,7 +14,12 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-
+          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<Homepage />} />
+          <Route path="/catering" element={<Catering />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Routes>
         <Footer />
       </BrowserRouter>
