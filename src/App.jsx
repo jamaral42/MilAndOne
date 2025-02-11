11041a3cf3c33ddf1ef4bf4bf0f0faaccf582ff5
João Amaral <joaomramaral42@gmail.com>
2025-02-11 16:31:37 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Homepage from './pages/Homepage';
 import Catering from './pages/Catering';
 import Portfolio from './pages/Portfolio';
 import About from './pages/About';
@@ -14,8 +13,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="*" element={<Homepage />} />
+
           <Route path="/catering" element={<Catering />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/about" element={<About />} />
